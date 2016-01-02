82dd02a9e3665cc805b83ee08e21d4691022de62
Kiran Kumar Chaudhary <hellokiran2020@gmail.com>
2016-01-02 16:18:29 +0545

    Facebook LogIn view added.

--- app/src/main/java/com/nnews/android/MainActivityFragment.java
+++ app/src/main/java/com/nnews/android/MainActivityFragment.java
@@ -6,6 +6,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.facebook.FacebookSdk;
+import com.facebook.login.widget.LoginButton;
+
 /**
  * A placeholder fragment containing a simple view.
  */
@@ -17,6 +20,12 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView =  inflater.inflate(R.layout.fragment_main, container, false);
+
+        LoginButton loginButton = (LoginButton) rootView.findViewById(R.id.login_button);
+
+        loginButton.setFragment(this);
+
+        return rootView;
     }
 }
